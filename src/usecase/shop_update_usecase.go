3536d17c4218508cefa4901bd0bcaedf07c387e7
HafishD <hafishdestarky@gmail.com>
2023-03-13 15:20:49 +0900

    update機能追加

---
+++ src/usecase/shop_update_usecase.go
@@ -0,0 +1,39 @@
+package usecase
+
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+	"github.com/takeru-a/halal_backend/lib"
+	// "github.com/takeru-a/halal_backend/models"
+)
+
+type UpdateShopUsecase struct {
+	shopRepo lib.ShopRepository
+}
+
+func NewUpdateShopUsecase(
+	shopRepo lib.ShopRepository,
+) *UpdateShopUsecase {
+	return &UpdateShopUsecase{
+		shopRepo: shopRepo,
+	}
+}
+
+func (usecase *UpdateShopUsecase) Execute(
+	ctx *gin.Context,
+	id string,
+	name string,
+	introduction string,
+	location string,
+	level int,
+	score float64,
+) error {
+	exist, _ := usecase.shopRepo.FindById(ctx, id)
+	if exist == nil {
+		return errors.New("That shop doesn't exist.")
+	}
+	
+	upd := exist.Update(name, introduction, location, level, score )
+	return upd
+}
